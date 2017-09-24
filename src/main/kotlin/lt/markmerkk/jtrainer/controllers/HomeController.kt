b989e6e575f7b0085be8f975ca0fe226e5561e27
Marius Merkevicius <marius.merkevicius@wahanda.com>
2017-09-24 13:47:41 +0300

    Working MD parser

--- src/main/kotlin/lt/markmerkk/jtrainer/controllers/HomeController.kt
+++ src/main/kotlin/lt/markmerkk/jtrainer/controllers/HomeController.kt
@@ -3,6 +3,13 @@ package lt.markmerkk.jtrainer.controllers
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.servlet.ModelAndView
+import com.vladsch.flexmark.ast.Node;
+import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
+import com.vladsch.flexmark.ext.tables.TablesExtension;
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.options.MutableDataSet;
 
 
 @Controller
@@ -14,6 +21,33 @@ class HomeController {
     )
     fun index(): String {
         return "index"
+    }
+
+    @RequestMapping(
+            value = *arrayOf("/test"),
+            method = arrayOf(RequestMethod.GET)
+    )
+    fun test(): ModelAndView {
+        val data = ModelAndView()
+
+        val options = MutableDataSet()
+
+        // uncomment to set optional extensions
+        //options.set(Parser.EXTENSIONS, Arrays.asList(TablesExtension.create(), StrikethroughExtension.create()));
+
+        // uncomment to convert soft-breaks to hard breaks
+        //options.set(HtmlRenderer.SOFT_BREAK, "<br />\n");
+
+        val parser = Parser.builder(options).build()
+        val renderer = HtmlRenderer.builder(options).build()
+
+        // You can re-use parser and renderer instances
+        val document = parser.parse("This is *Sparta*")
+        val html = renderer.render(document)  // "<p>This is <em>Sparta</em></p>\n"
+
+
+        data.addObject("out", html)
+        return data
     }
 
 }

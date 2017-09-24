06099be7800f55656f3e5b2df5a64426c7a26d67
Marius Merkevicius <marius.merkevicius@wahanda.com>
2017-09-24 16:52:28 +0300

    Working trainings integration

--- src/main/kotlin/lt/markmerkk/jtrainer/controllers/HomeController.kt
+++ src/main/kotlin/lt/markmerkk/jtrainer/controllers/HomeController.kt
@@ -1,19 +1,16 @@
 package lt.markmerkk.jtrainer.controllers
 
+import com.vladsch.flexmark.html.HtmlRenderer
+import com.vladsch.flexmark.parser.Parser
+import com.vladsch.flexmark.util.options.MutableDataSet
+import org.apache.commons.io.IOUtils
+import org.springframework.core.io.ClassPathResource
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.servlet.ModelAndView
-import com.vladsch.flexmark.ast.Node;
-import com.vladsch.flexmark.ext.gfm.strikethrough.StrikethroughExtension;
-import com.vladsch.flexmark.ext.tables.TablesExtension;
-import com.vladsch.flexmark.html.HtmlRenderer;
-import com.vladsch.flexmark.parser.Parser;
-import com.vladsch.flexmark.util.options.MutableDataSet;
-import org.apache.commons.io.IOUtils
-import org.springframework.core.io.ClassPathResource
 import java.io.StringWriter
-import org.springframework.context.support.ClassPathXmlApplicationContext
 
 @Controller
 class HomeController {
@@ -22,24 +19,21 @@ class HomeController {
             value = *arrayOf("/"),
             method = arrayOf(RequestMethod.GET)
     )
-    fun index(): String {
-        return "index"
-    }
+    fun index(
+            @RequestParam(required = false) inputDoc: String?
+    ): ModelAndView {
+        val data = ModelAndView("index")
 
-    @RequestMapping(
-            value = *arrayOf("/test"),
-            method = arrayOf(RequestMethod.GET)
-    )
-    fun test(): ModelAndView {
-        val data = ModelAndView()
+        val docName = if (!inputDoc.isNullOrEmpty()) {
+            inputDoc
+        } else {
+            "index"
+        }
 
         val options = MutableDataSet()
-
         val parser = Parser.builder(options).build()
         val renderer = HtmlRenderer.builder(options).build()
-
-        // You can re-use parser and renderer instances
-        val resource = ClassPathResource("/assets/help.md")
+        val resource = ClassPathResource("/assets/$docName.md")
         val resourceInputStream = resource.inputStream
         val writer = StringWriter()
         IOUtils.copy(resourceInputStream, writer, "UTF-8")
